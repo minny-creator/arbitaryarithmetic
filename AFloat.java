3aa8f8774e0caea0c76f9e4bddd1d07c6e5fb0a4
minny-creator <ssailolitha@gmail.com>
2026-07-05 12:59:48 +0530

    Refactor AFloat class for improved functionality
    Refactor AFloat class methods for better readability and functionality. Update constructor and arithmetic methods to handle floating-point operations more effectively.

--- AFloat.java
+++ AFloat.java
@@ -6,7 +6,7 @@ public class AFloat{
 
 
     public AFloat(){
-        this.AFloat="0";
+        this.AFloat="0.0";
     }
 
 
@@ -22,217 +22,149 @@ public class AFloat{
 
 
     public AFloat addAFloat(AFloat a,AFloat b){
+        
         int d=a.AFloat.length();
         int e=b.AFloat.length();
         int i;
-        int k=0;
+        int k_d=0;
         for(i=0;i<d;i++){
             if(a.AFloat.substring(i,i+1).equals(".")){
-                k=k+1;
+                k_d=i;
                 break;
             }
         }
-        AInteger a1=new AInteger();
-        AInteger a2=new AInteger();
-        if (i!=(d-1)){
-            a1.AInteger=(a.AFloat.substring(0,i));
-            a2.AInteger=(a.AFloat.substring(i+1,d));
-        }
-        else{
-            if(k==1){
-                a1.AInteger=(a.AFloat.substring(0,i));
-                a2.AInteger="0";
-            }
-            else{
-                a1.AInteger=(a.AFloat.substring(0,i+1));
-                a2.AInteger="0";
-            }
-        }
-        int d2=a2.AInteger.length();
-        k=0;
+        int k_e=0;
         for(i=0;i<e;i++){
-            if(b.AFloat.substring(i, i+1).equals(".")){
-                k=k+1;
+            if(b.AFloat.substring(i,i+1).equals(".")){
+                k_e=i;
                 break;
             }
         }
-        AInteger b1=new AInteger();
-        AInteger b2=new AInteger();
-        if (i!=(e-1)){
-            b1.AInteger=(b.AFloat.substring(0,i));
-            b2.AInteger=(b.AFloat.substring(i+1,e));
+        if(d-k_d>e-k_e){
+            for(i=1;i<=(d-k_d-(e-k_e));i++){
+                b.AFloat=b.AFloat+"0";
+            }
+            AInteger a1 = new AInteger(a.substring(0,k_d));
+            AInteger b1 = new AInteger(b.substring(0,k_d));
+            a1.AInteger=a1.AInteger+a.substring(k_d+1);
+            b1.AInteger=b1.AInteger+b.substring(k_d+1);
         }
         else{
-            if(k==1){
-                b1.AInteger=(b.AFloat.substring(0,i));
-                b2.AInteger="0";
+            for(i=1;i<=e-k_e-(d-k_d);i++){
+                a.AFloat=a.AFloat+"0";
             }
-            else{
-                b1.AInteger=(b.AFloat.substring(0,i+1));
-                b2.AInteger="0";
-            }
+            AInteger a1 = new AInteger(a.substring(0,k_e));
+            AInteger b1 = new AInteger(b.substring(0,k_e));
+            a1.AInteger=a1.AInteger+a.substring(k_e+1);
+            b1.AInteger=b1.AInteger+b.substring(k_e+1);
+            k_d=k_e;
+            d=e;
         }
-        int e2=b2.AInteger.length();
-        if(d2>e2){
-            int j=(d2-e2);
-            for(i=0;i<j;i++){
-                b2.AInteger=b2.AInteger+"0";
-            }
-            i=d2;
-        }
-        else{
-            int j=(e2-d2);
-            if(j!=0){
-                for(i=0;i<j;i++){
-                    a2.AInteger=a2.AInteger+"0";
-                }
-            }
-            i=e2;
-        }
-        a1.AInteger=a1.AInteger+a2.AInteger;
-        b1.AInteger=b1.AInteger+b2.AInteger;
-        a2=a1.addAIntegers(a1,b1);
-        int e3=a2.AInteger.length();
-        int d3=(e3-i);
-        a.AFloat=a2.AInteger.substring(0,d3)+"."+a2.AInteger.substring(d3,e3);
+        a1 = addAIntegers(a1,b1);
+        //d-k_d-1
+        a.AFloat="";
+        a.AFloat=a.AFloat+a1.substring(0,a1.AInteger.length()-(d-k_d-1));
+        a.AFloat=a.AFloat+".";
+        a.AFloat=a.AFloat+a1.substring(a1.AInteger.length()-(d-k_d-1));
         return a;
+    
     }
 
 
     public AFloat subAFloat(AFloat a,AFloat b){
+
         int d=a.AFloat.length();
         int e=b.AFloat.length();
         int i;
-        int k=0;
+        int k_d=0;
         for(i=0;i<d;i++){
             if(a.AFloat.substring(i,i+1).equals(".")){
-                k=k+1;
+                k_d=i;
                 break;
             }
         }
-        AInteger a1=new AInteger();
-        AInteger a2=new AInteger();
-        if (i!=(d-1)){
-            a1.AInteger=(a.AFloat.substring(0,i));
-            a2.AInteger=(a.AFloat.substring(i+1,d));
-        }
-        else{
-            if(k==1){
-                a1.AInteger=(a.AFloat.substring(0,i));
-                a2.AInteger="0";
-            }
-            else{
-                a1.AInteger=(a.AFloat.substring(0,i+1));
-                a2.AInteger="0";
-            }
-        }
-        int d2=a2.AInteger.length();
-        k=0;
+        int k_e=0;
         for(i=0;i<e;i++){
-            if(b.AFloat.substring(i, i+1).equals(".")){
-                k=k+1;
+            if(b.AFloat.substring(i,i+1).equals(".")){
+                k_e=i;
                 break;
             }
         }
-        AInteger b1=new AInteger();
-        AInteger b2=new AInteger();
-        if (i!=(e-1)){
-            b1.AInteger=(b.AFloat.substring(0,i));
-            b2.AInteger=(b.AFloat.substring(i+1,e));
+        if(d-k_d>e-k_e){
+            for(i=1;i<=(d-k_d-(e-k_e));i++){
+                b.AFloat=b.AFloat+"0";
+            }
+            AInteger a1 = new AInteger(a.substring(0,k_d));
+            AInteger b1 = new AInteger(b.substring(0,k_d));
+            a1.AInteger=a1.AInteger+a.substring(k_d+1);
+            b1.AInteger=b1.AInteger+b.substring(k_d+1);
         }
         else{
-            if(k==1){
-                b1.AInteger=(b.AFloat.substring(0,i));
-                b2.AInteger="0";
+            for(i=1;i<=e-k_e-(d-k_d);i++){
+                a.AFloat=a.AFloat+"0";
             }
-            else{
-                b1.AInteger=(b.AFloat.substring(0,i+1));
-                b2.AInteger="0";
-            }
+            AInteger a1 = new AInteger(a.substring(0,k_e));
+            AInteger b1 = new AInteger(b.substring(0,k_e));
+            a1.AInteger=a1.AInteger+a.substring(k_e+1);
+            b1.AInteger=b1.AInteger+b.substring(k_e+1);
+            k_d=k_e;
+            d=e;
         }
-        int e2=b2.AInteger.length();
-        if(d2>e2){
-            int j=(d2-e2);
-            for(i=0;i<j;i++){
-                b2.AInteger=b2.AInteger+"0";
-            }
-            i=d2;
+        a1 = subAIntegers(a1,b1);
+        //d-k_d-1
+        if(a1.AInteger.length()==d-k_d-1){
+            if(a1.AInteger[0]=="-"){a.AFloat="-0.0";a1.AInteger=a1.AInteger.substring(1);}
+            else a.AFloat="0.";
+            a.AFloat=a.AFloat+a1.AInteger;            
+        }
+        else if(a1.AInteger.length()>d-k_d-1){
+            a.AFloat=a1.substring(0,a1.AInteger.length()-(d-k_d-1));
+            a.AFloat=a.AFloat+".";
+            a.AFloat=a.AFloat+a1.substring(a1.AInteger.length()-(d-k_d-1));
         }
         else{
-            int j=(e2-d2);
-            if(j!=0){
-                for(i=0;i<j;i++){
-                    a2.AInteger=a2.AInteger+"0";
-                }
+            if(a1.AInteger[0]=="-"){a.AFloat="-0.";a1.AInteger=a.AInteger.substring(1);}
+            else a.AFloat="0.";
+            for(i=1;i<=((d-k_d-1)-a.AInteger.length());i++){
+                a1.AInteger="0"+a1.AInteger;
             }
-            i=e2;
+            a.AFloat=a.AFloat+a1.AInteger;
         }
-        a1.AInteger=a1.AInteger+a2.AInteger;
-        b1.AInteger=b1.AInteger+b2.AInteger;
-        a2=a1.subAIntegers(a1,b1);
-        e=a2.AInteger.length();
-        d=(e-i);
-        a.AFloat=a2.AInteger.substring(0,d)+"."+a2.AInteger.substring(d,e);
         return a;
+       
     }
 
 
     public AFloat mulAFloat(AFloat a,AFloat b){
+
         int d=a.AFloat.length();
         int e=b.AFloat.length();
         int i;
-        int k=0;
+        int k_d=0;
         for(i=0;i<d;i++){
             if(a.AFloat.substring(i,i+1).equals(".")){
-                k=k+1;
+                k_d=i;
                 break;
             }
         }
-        AInteger a1=new AInteger();
-        if(i!=(d-1)){
-            a1.AInteger = a.AFloat.substring(0,i)+a.AFloat.substring(i+1,d);
-        }
-        else{
-            if(k==1){
-                a1.AInteger = a.AFloat.substring(0,i);
-            }
-            else{
-                a1.AInteger = a.AFloat.substring(0,i+1);
-            }
-        }
-        i=(d-i-1);
-        int j;
-        k=0;
-        for(j=0;j<d;j++){
+        int k_e=0;
+        for(i=0;i<e;i++){
             if(b.AFloat.substring(i,i+1).equals(".")){
-                k=k+1;
+                k_e=i;
                 break;
             }
         }
-        AInteger b1=new AInteger();
-        if(j!=(e-1)){
-            b1.AInteger=b.AFloat.substring(0,j)+b.AFloat.substring(j+1,e);
-        }
-        else{
-            if(k==1){
-                b1.AInteger = b.AFloat.substring(0,i);
-            }
-            else{
-                b1.AInteger = b.AFloat.substring(0,i+1);
-            }
-        }
-        
-        j=(e-j-1);
-        a1=a1.mulAIntegers(a1, b1);
-        d=a1.AInteger.length();
-        if((i+j)!=0){
-            a.AFloat=a1.AInteger.substring(0,(d-i-j))+"."+a1.AInteger.substring((d-i-j),d);
-        }
-        else{
-            a.AFloat=a1.AInteger.substring(0,d);
-        }
-        
+        AInteger a1 = new AInteger(a.substring(0,k_d));
+        AInteger b1 = new AInteger(b.substring(0,k_e));
+        a1.AInteger=a1.AInteger+a.substring(k_d+1);
+        b1.AInteger=b1.AInteger+b.substring(k_e+1);
+        a1 = mulAIntegers(a1,b1);
+        //d-k_d-1+e-k_e-1=total numbers after "."
+        a.AFloat=a1.substring(0,a1.AInteger.length()-d-k_d-1+e-k_e-1);
+        a.AFloat=a.AFloat+".";
+        a.AFloat=a.AFloat+a1.substring(a1.AInteger.length()-d-k_d-1+e-k_e-1);
         return a;
+        
     }
 
 
